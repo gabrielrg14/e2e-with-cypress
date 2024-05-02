fb79a777a588b2d58670fabdb320a28e5d3190c3
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-05-02 17:17:45 -0300

    Configure Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
-	chromeWebSecurity: false,
+	projectId: "sxyvst",
 	e2e: {
 		baseUrl: "https://notes-serverless-app.com",
 		requestTimeout: 10000,
 		env: {
 			viewportWidthBreakpoint: 768
 		}
-	}
+	},
+	chromeWebSecurity: false
 })
