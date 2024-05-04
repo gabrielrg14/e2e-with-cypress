435bb9ea37f223f182a2b36c6f07f1b426aaf218
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-05-04 00:47:38 -0300

    Fix lint issue

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -34,7 +34,7 @@ describe("Scenarios where authentication is a pre-condition", () => {
 
 	it("logs out", { tags: "@desktop-and-mobile" }, () => {
 		cy.visit("/")
-		cy.wait(2000)
+		cy.wait("@getNotes")
 
 		cy.contains(".nav a", "Logout").click()
 
